b5c7dcf4ba679d1221e73d3c7edefe898bbdc602
tuan <nhtuan10@gmail.com>
2025-09-17 23:29:12 -0400

    change button colors and styles

--- my-kafka-tool-main/src/main/java/io/github/nhtuan10/mykafkatool/constant/UIStyleConstant.java
+++ my-kafka-tool-main/src/main/java/io/github/nhtuan10/mykafkatool/constant/UIStyleConstant.java
@@ -13,6 +13,7 @@ public class UIStyleConstant {
     public static final String CLUSTER_TREE_ITEM_CLASS = "cluster-tree-item";
     public static final String POLL_MSG_BTN_CLASS = "success";
     public static final String STOP_POLLING_MSG_BTN_CLASS = "danger";
+    public static final String BUTTON_OUTLINE_CLASS = "button-outlined";
 
     public static String getCssThemeFile() {
         return LIGHT_STYLE_CSS_FILE;
